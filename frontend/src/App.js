8bdc82e3057ad0e0bc3d2b73c068a59407c36a75
Pavithra V <pavithra.v@syskeysoftlabs.com>
2022-11-25 14:31:33 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import Navigation from "./components/Navigation";
-import * as sessionActions from "./store/session";
-import Spots from "./components/Spots";
-import SpotByID from "./components/SpotByID";
-import MyReviews from "./components/MyReviews";
-import MyListings from "./components/MyListings";
-import BookingConfirmation from "./components/BookingConfirmation";
-import MyBookings from "./components/MyBookings";
-import Footer from "./components/Footer";
+import React from 'react';
+import './App.css';
+
+import logo from './assets/logo.svg'
+
+import Routes from './routes'
 
 function App() {
-  const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
-  }, [dispatch]);
-
   return (
-    <>
-      <Navigation isLoaded={isLoaded} />
-      <Switch>
-        <Route exact path={["/", "/listings"]}>
-          <Spots />
-        </Route>
-        <Route exact path="/listings/:id">
-          <SpotByID />
-        </Route>
-        <Route path='/users/:id/bookings'>
-          <MyBookings />
-        </Route>
-        <Route path="/users/:id/listings">
-          <MyListings />
-        </Route>
-        <Route path="/users/:id/reviews">
-          <MyReviews />
-        </Route>
-        <Route path='/listings/:spotId/bookings/:id'>
-          <BookingConfirmation />
-        </Route>
-        <Route>Sorry resource not found. Please check url</Route>
-      </Switch>
-      <Footer />
-    </>
+      <div className="container">
+        <img src={logo} alt="aircnc"/>
+        <div className="content">
+          <Routes/>
+        </div>
+      </div>
   );
 }
 
